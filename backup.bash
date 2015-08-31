5dcbde1642fd2f6c274b13a67cf3e6d356152def
funkytwig <ben@funkytwig.com>
2015-08-31 21:42:29 +0100

    Check disk space and send email if low

--- backup.bash
+++ backup.bash
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash 
 #
 # back.bash - Main funkybackup script
 #
@@ -22,6 +22,29 @@ seq=`date +%s` # julian seconds
 new_backup=$path_to_store_backups/${backup_name}_${seq}_F_${date}
 
 log "Creating backup $new_backup from $what_to_backup"
+
+# Cheching disk space
+
+df_info=`df -h $path_to_store_backups| tail -1`
+
+disk_total=`echo $df_info | awk '{print $2}'`
+disk_used=`echo $df_info | awk '{print $3}'`
+disk_free=`echo $df_info | awk '{print $4}'`
+disk_percent=`echo $df_info | awk '{print $5}' `
+disk_percent=${disk_percent::-1}
+
+disk_info="$path_to_store_backups, total=$disk_total, used=$disk_used, free=$disk_free, used=${disk_percent}%"
+
+if [ $disk_percent -lt $disk_low_warning_percent ]; then
+  log "$disk_info (threshold=$disk_low_warning_percent%)"
+else
+  body="$disk_info"
+  temp_file="/tmp/$$_disk_space_email.tml"
+  echo "Low threshhold set to $disk_low_warning_percent%"	>> $temp_file
+  echo "" 							>> $temp_file
+  echo "Consider deleting old backups"  			>> $temp_file
+  send_email "Disk space geting low (${disk_percent}%) on `hostname`:$path_to_store_backups($backup_name)" "$body" "$temp_file"
+fi
 
 # run rsync
 
