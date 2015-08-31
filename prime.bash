76a2397aa481a45448f2dcc13b6f019dfb4857a8
funkytwig <ben@funkytwig.com>
2015-08-31 16:08:18 +0100

    Place quotes around varables so filemanes can have spaces

--- prime.bash
+++ prime.bash
@@ -10,17 +10,17 @@ source function.inc.bash
 
 date=`date "+%a_%d%b%Y_%H%M"`
 seq=`date +%s` # julian seconds
-new_backup=$path_to_store_backups/${backup_name}_${seq}_F_${date}
+new_backup="$path_to_store_backups/${backup_name}_${seq}_F_${date}"
 
 echo  "Empty backup dir=$new_backup, symlink=$latest_backup"
 
-mkdir $new_backup
+mkdir "$new_backup"
 
-ln -s $new_backup $latest_backup
+ln -s "$new_backup $latest_backup"
 
-ls -l $path_to_store_backups
+ls -l "$path_to_store_backups"
 
 echo "Creating logfile, you may need to type password for sudo"
 
-sudo touch $logfile
-sudo chown $user.$user $logfile
+sudo touch "$logfile"
+sudo chown "$user.$user $logfile"
