bb70b7352ce27c8f59d1dee977cb0d30e840cc61
funkytwig <ben@funkytwig.com>
2015-08-31 14:47:25 +0100

    tidy up source formatting
    modified:   function.inc.bash
    modified:   start.bash

--- function.inc.bash
+++ function.inc.bash
@@ -11,62 +11,62 @@
 # 0.5   Initial version
 
 function log {
-	log_text="$logstamp $basename $1"
+  log_text="$logstamp $basename $1"
 
-	if [ $interactive -eq 1 ]; then
-		echo  $log_text
-	else
-		echo "$logstamp $basename($$) $1" >> $logfile
-	fi
+  if [ $interactive -eq 1 ]; then
+    echo  $log_text
+  else
+    echo "$logstamp $basename($$) $1" >> $logfile
+  fi
 }
 
 function log_file {
-	while read line 
-	do
-		log "$line" 
-	done < "$1"
+  while read line 
+  do
+    log "$line" 
+  done < "$1"
 }
 
 function run_cmd {
-	tmp_log=/tmp/$$_cmd.log
+  tmp_log=/tmp/$$_cmd.log
 
-	log "$1"
+  log "$1"
 
-	$1 > $tmp_log 2>&1
+  $1 > $tmp_log 2>&1
 
-	ret=$?
+  ret=$?
 
-	if [ -f $tmp_log ]; then
-  		log_file $tmp_log
-	fi
+  if [ -f $tmp_log ]; then
+    log_file $tmp_log
+  fi
 
-#	if [ $ret -ne 0 ]; then
-#	  error "$ret : $1"
-#	fi
+  if [ $ret -ne 0 ]; then
+    log "$ret : $1"
+  fi
 }
 
 function send_email {
-	subject="$1"
-	message="$2"
-	file="$3"
+  subject="$1"
+  message="$2"
+  file="$3"
 
-	temp_file=/tmp/$0_$$.send_email.txt
+  temp_file=/tmp/$0_$$.send_email.txt
 
-	log "Sendind subject=$subject, message=$message, file=$file"
+  log "Sendind subject=$subject, message=$message, file=$file"
 
-	echo $message >> $temp_file
+  echo $message >> $temp_file
 
-	if [ ! -z $file ]; then # $file set
-  		echo		>> $temp_file
-  		cat $file	>> $temp_file
-	fi
+  if [ ! -z $file ]; then # $file set
+    echo	>> $temp_file
+    cat $file	>> $temp_file
+  fi
 
-	mail -s "$subject" $to_email < $temp_file
+  mail -s "$subject" $to_email < $temp_file
 
-	file="" # in case this is called twice
+  file="" # in case this is called twice
 }
 
 function error {
-	log "ERROR $1"
-	send_email "Error from $0" "$1"
+  log "ERROR $1"
+  send_email "Error from $0" "$1"
 }
--- start.bash
+++ start.bash
@@ -2,29 +2,28 @@
 #
 
 function check_dir_exists {
-	directory=$1
-	total_down_min=0;
-	loop_pass_since_error=0 # a link pass is actualy a minute as the wait it 60 (seconds)
+  directory=$1
+  total_down_min=0;
+  loop_pass_since_error=0 # a link pass is actualy a minute as the wait it 60 (seconds)
 
-	while [ ! -d "$directory" ]
-	do
-		touch waiting.tmp
-		lock_pass_since_error=$((lock_pass+1))
-		sleep 60
-		if [ ! -d $directory ]; then
-			total_down_min=$((total_down_min+1))
-	       		log "$directory down for 60 seconds"
+  while [ ! -d "$directory" ]
+  do
+    touch waiting.tmp
+    lock_pass_since_error=$((lock_pass+1))
+    sleep 60
+    if [ ! -d $directory ]; then
+      total_down_min=$((total_down_min+1))
+      log "$directory down for 60 seconds"
 
-       			if [ $lock_pass_since_error -gt $link_down_max_min ]; then
-	        		error "$directory not accessable for more then $total_down_min minutes"
-			fi
- 			loop_pass_since_error=0
-		else
-			# finished waiting
-			rm waiting.tmp
-		fi
-        done
-
+      if [ $lock_pass_since_error -gt $link_down_max_min ]; then
+        error "$directory not accessable for more then $total_down_min minutes"
+      fi
+      loop_pass_since_error=0
+    else
+    # finished waiting
+      rm waiting.tmp
+    fi
+  done
 }
 
 # if another script is waiting for a directory to become available (check_dir_exists) exit script otherwise
@@ -58,13 +57,14 @@ lock_pass=0
 
 while [ -f backup.lock.tmp ] 
 do
-        lock_pass=$((lock_pass+1))
-	log "Waiting 15 seconds for lock (pass $lock_pass)"
-        sleep 15
-        if [ $lock_pass -gt 60 ]; then
-                error "Waiting for lock for more than 15 minutes" 
-                exit 1
-        fi
+  lock_pass=$((lock_pass+1))
+  log "Waiting 15 seconds for lock (pass $lock_pass)"
+  sleep 15
+
+  if [ $lock_pass -gt 60 ]; then
+    error "Waiting for lock for more than 15 minutes" 
+    exit 1
+  fi
 done
 
 touch backup.lock.tmp
