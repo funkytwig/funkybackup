c10d6871939e3a086b5a06d57ed5695f169f6288
funkytwig <ben@funkytwig.com>
2015-09-02 18:25:33 +0100

    Add script to ensure stuff stays mounted

---
+++ keep_mounted.bash
@@ -0,0 +1,20 @@
+#!/bin/bash
+#
+# back.bash - Main funkybackup script
+#
+# For more details about the script and to me goto 
+# http://www.funkytwig.com/blog/funkybackup
+#
+# (c) 2015 Ben Edwards (funkytwig.com) 
+# You are alowed to use the script if you keep this head0er 
+# and do not redistibute it, just send people to the URL
+#
+# This is run as root every minute 9or less often) to try to remount drives if lost
+
+source vars.inc.bash
+source function.inc.bash
+
+if [ ! -d $what_to_backup ] || [ ! -d $path_to_store_backups ]; then
+  log "Atemting to remount(`pwd`)"
+  mount -a
+fi
