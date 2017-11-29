7472443363d9fd3c67d2c932e8d101dcaa1aeb92
funkytwig <ben@funkytwig.com>
2017-11-29 19:56:27 +0000

    Change script so it uses ssh (not script is run on backup server).  Also add fix_backup script if  the latest directory is wrong.

--- start.bash
+++ start.bash
@@ -3,36 +3,12 @@
 
 function check_dir_exists {
   directory=$1
-  total_down_min=0;
-  loop_pass_since_error=0 # a link pass is actualy a minute as the wait it 60 (seconds)
 
-  while [ ! -d "$directory" ]
-  do
-    touch $waitfile
-    lock_pass_since_error=$((lock_pass+1))
-    sleep 60
-    if [ ! -d $directory ]; then
-      total_down_min=$((total_down_min+1))
-      log "$directory down for 60 seconds"
-
-      if [ $lock_pass_since_error -gt $link_down_max_min ]; then
-        error "$directory not accessable for more then $total_down_min minutes"
-      fi
-      loop_pass_since_error=0
-    else
-    # finished waiting
-      rm $waitfile
-    fi
-  done
+  if [ ! -d "$directory" ]; then
+        error "$directory not does not exits, exiting"
+  	exit 1;
+  fi
 }
-
-# if another script is waiting for a directory to become available (check_dir_exists) exit script otherwise
-# cron will spawn lots of scripts that will all run at once.
-
-if [ -f $waitfile ]; then
-  log "EXITING, Another script is waiting for directory to become available"
-  exit 1
-fi
 
 # make sure all varables set in vars.inc.bash
 
@@ -53,19 +29,10 @@ fi
 
 # make sure onle one script is running at once
 
-lock_pass=0
-
-while [ -f $lockfile ] 
-do
-  lock_pass=$((lock_pass+1))
-  log "Waiting 15 seconds for lock (pass $lock_pass)"
-  sleep 15
-
-  if [ $lock_pass -gt 60 ]; then
-    error "Waiting for lock for more than 15 minutes" 
-    exit 1
-  fi
-done
+if [ -f $lockfile ]; then
+  error "Another script is running ($lockfile exists), exiting"
+  exit 1;
+fi 
 
 # ensure backup directory is present, if it is not this may be it is a USB drive and use total_down_min to warn if backup
 # has not been done for a while. This will also warn if link has got broked, if this hapens the backup will become a full
@@ -73,7 +40,7 @@ done
 # just missing but for now both are treated the same.  A auto repair can be added to recreate link if it is missing (i.e.
 # does not point to a calid directory).
 
-check_dir_exists $what_to_backup
+#check_dir_exists $what_to_backup
 check_dir_exists $path_to_store_backups
 
 touch $lockfile
